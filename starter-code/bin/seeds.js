04bacc921cc2e887da60f2018ee2dec11183c5cb
Julio García Pérez <juliojose.garcia@11paths.com>
2017-11-08 23:21:03 +0100

    First commit

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev', {useMongoClient: true});
+const Drone = require('../models/drone');
+
+// Reset Data
+
+Drone.collection.drop();
+
 // Iteration #1
+
+const droneData = [
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(droneData, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone.droneName);
+  })
+
+  mongoose.connection.close();
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Drone = require('../models/drone');
 
 // require the Drone model here
 
@@ -6,16 +7,37 @@ const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err) { return next(err) }
+
+    res.render('drones/index', {
+      drones: drones
+    });
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+  res.render('drones/new');
 });
 
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+  const droneInfo = {
+      name: req.body.name,
+      price: req.body.price,
+      imageUrl: req.body.imageUrl,
+      description: req.body.description
+  };
+
+  new Drone({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  }).save( (err) => {
+    if (err) { return next(err) }
+
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
